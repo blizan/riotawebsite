17d2571150c61125fc671b2eac6ed5d0b8d11b7a
Rafael Mendiola <dslounge@gmail.com>
2019-02-25 14:27:18 -0500

    more updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,8 @@ export default class IndexPage extends Component {
     this.state = {
       adjectiveIndex: 0,
     }
+
+    this.renderHeader = this.renderHeader.bind(this)
   }
 
   componentDidMount() {
@@ -28,96 +30,125 @@ export default class IndexPage extends Component {
     }, SECONDS * 1000)
   }
 
-  render() {
+  renderHeader() {
     const { adjectiveIndex } = this.state
     const { label, color } = adjectives[adjectiveIndex]
 
     return (
+      <div className="header">
+        <div className="nameRow">
+          <img src={logo} className="logo" />
+        </div>
+        <h1>
+          Take your hotel to the{" "}
+          <span
+            style={{
+              color,
+              display: "inline-block",
+              width: "95px",
+              textAlign: "center",
+            }}
+          >
+            {label}
+          </span>{" "}
+          era.
+        </h1>
+        <div className="subheaders">
+          <div className="subheader">
+            <h2>
+              Riota helps hotels to save 35% of their in-room energy &amp; water
+              expenditures
+            </h2>
+          </div>
+          <div className="subheader">
+            <h2>
+              Riota helps hotels to increase revenue and increase direct
+              bookings.
+            </h2>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderContact() {
+    return (
+      <div className="contact">
+        <h1>Contact</h1>
+
+        <h2>We would love to get in touch. Let us know how to:</h2>
+        <form>
+          <div>
+            <label>Name</label>
+            <input />
+          </div>
+          <div>
+            <label>Business Email</label>
+            <input />
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  render() {
+    return (
       <div className="main">
         <SEO title="Home" keywords={[`riota`]} />
+        {this.renderHeader()}
+        {this.renderContact()}
 
-        <div className="header">
-          <div className="nameRow">
-            <img src={logo} className="logo" />
-          </div>
-          <h1>
-            Take your hotel to the{" "}
-            <span
-              style={{
-                color,
-                display: "inline-block",
-                width: "95px",
-                textAlign: "center",
-              }}
-            >
-              {label}
-            </span>{" "}
-            era.
-          </h1>
-          <div className="subheaders">
-            <div className="subheader">
-              <h2>
-                Riota helps hotels to save 35% of their in-room energy &amp;
-                water expenditures
-              </h2>
-            </div>
-            <div className="subheader">
-              <h2>
-                Riota helps hotels to increase revenue and increase direct
-                bookings.
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="section">
-          <h1>Contact</h1>
-
-          <h2>We would love to get in touch. Let us know how to:</h2>
-          <form>
-            <div>
-              <label>Name</label>
-              <input />
-            </div>
-            <div>
-              <label>Business Email</label>
-              <input />
-            </div>
-          </form>
-        </div>
-        <div className="section">
+        <div className="sellPoints">
           <h1>How it works</h1>
-          <div className="imageBlock">
-            <img src={hotelRoom} />
+          <div className="section">
+            <div className="imageBlock">
+              <img src={hotelRoom} />
+            </div>
+            <div className="sectionText">
+              <h2>
+                Riota sets up a network of sticker sensors on the hotel room
+                that measures energy and water consumption in real time.
+              </h2>
+            </div>
           </div>
-          <h2>
-            Riota sets up a network of sticker sensors on the hotel room that
-            measures energy and water consumption in real time.
-          </h2>
-        </div>
-        <div className="section">
-          <div className="imageBlock">{}</div>
-          <h2>
-            Guests are offered rewards when consuming less energy and water than
-            average.
-          </h2>
-          <ul>
-            <li>Bar/Restaurant vouchers</li>
-            <li>Discounts for future stays when booked direct</li>
-          </ul>
-        </div>
-        <div className="section">
-          <div className="imageBlock">{}</div>
-          <h2>
-            Guest redeem their prizes, enhancing their guest experience and
-            generating upselling opportunities
-          </h2>
-        </div>
-        <div className="section">
-          <div className="imageBlock">{}</div>
-          <h2>
-            Riota manages the whole process to ensure a smooth experience from
-            both the guest and the staff, being integrated with their routine
-          </h2>
+          <div className="section reverse">
+            <div className="imageBlock">
+              <img src={hotelRoom} />
+            </div>
+            <div className="sectionText">
+              <h2>
+                Guests are offered rewards when consuming less energy and water
+                than average.
+              </h2>
+              <ul>
+                <li>Bar/Restaurant vouchers</li>
+                <li>Discounts for future stays when booked direct</li>
+              </ul>
+            </div>
+          </div>
+          <div className="section">
+            <div className="imageBlock">
+              <img src={hotelRoom} />
+            </div>
+            <div className="sectionText">
+              <h2>
+                Guest redeem their prizes, enhancing their guest experience and
+                generating upselling opportunities
+              </h2>
+            </div>
+          </div>
+          <div className="section reverse">
+            <div className="imageBlock">
+              <img src={hotelRoom} />
+            </div>
+            <div className="sectionText">
+              <h2>
+                Riota manages the whole process to ensure a smooth experience
+                from both the guest and the staff, being integrated with their
+                routine
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
     )
