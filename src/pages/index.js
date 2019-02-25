3731b390fa553c37129674b31d7474150800bb25
Rafael Mendiola <dslounge@gmail.com>
2019-02-24 22:54:20 -0500

    rotate adjective

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,54 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import "./style.scss"
 
+const adjectives = [
+  { label: "digital", color: "#2a558a" },
+  { label: "green", color: "#6f8e2f" },
+]
+
 export default class IndexPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      adjectiveIndex: 0,
+    }
+  }
+
+  componentDidMount() {
+    const SECONDS = 10
+    setInterval(() => {
+      this.setState({
+        adjectiveIndex: (this.state.adjectiveIndex + 1) % adjectives.length,
+      })
+    }, SECONDS * 1000)
+  }
+
   render() {
+    const { adjectiveIndex } = this.state
+    const { label, color } = adjectives[adjectiveIndex]
+
     return (
       <div className="main">
-        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <SEO title="Home" keywords={[`riota`]} />
 
         <div className="header">
           <div style={{ width: 150 }}>
             <Image />
           </div>
-          <h2>Take your hotel to the digital (green) era.</h2>
+          <h1>
+            Take your hotel to the{" "}
+            <span
+              style={{
+                color,
+                display: "inline-block",
+                width: "95px",
+                textAlign: "center",
+              }}
+            >
+              {label}
+            </span>{" "}
+            era.
+          </h1>
           <div className="row">
             <div className="column">
               <h2>
