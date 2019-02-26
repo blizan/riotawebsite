87c71497ec64f63df4dba1c067b929711cd59275
Rafael Mendiola <dslounge@gmail.com>
2019-02-25 22:33:32 -0500

    Updates

---
+++ pages-manifest-91f981ca22efab33bc72.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{141:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","sq--src-components-image-js":2894418287,"sq--src-components-seo-js":1025518380}}}}]);
+//# sourceMappingURL=pages-manifest-91f981ca22efab33bc72.js.map
